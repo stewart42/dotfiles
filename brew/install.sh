24aa689da50d80ffab92c147390f3752025992a7
Stewart Scott <stewart42@gmail.com>
2020-06-06 23:13:32 +0200

    fix homebrew install

--- brew/install.sh
+++ brew/install.sh
@@ -1,4 +1,6 @@
 if test ! $(which brew) ; then
     echo "installing brew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    brew upgrade
 fi
